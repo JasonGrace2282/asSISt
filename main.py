4b3bbc8408887dcc4aed2be46b3e0d1f451a1622
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-24 15:43:22 -0500

    Lint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
- 
+from ui import auth
 
 
 class App(ctk.CTk):
@@ -8,8 +8,19 @@ class App(ctk.CTk):
         self.title("Hello World")
         self.geometry("500x500")
 
-        title1 = ctk.CTkLabel(self,text="Calculate the impact of an new individual assginment on your grade based on its weight and score")
-        title2 = ctk.CTkLabel(self,text="Instructions: Pick whether to enter your grade as number of right points or as a percentage.")
+        ctk.CTkLabel(
+            self,
+            text="Calculate the impact of an new individual assginment on your\
+                grade based on its weight and score".replace(
+                    "               ",
+                    ""
+                )
+        ).pack(pady=20)
+        ctk.CTkLabel(
+            self,
+            text="Instructions: Pick whether to enter your grade as number of\
+            right points or as a percentage.".replace("           ", "")
+        ).pack(pady=20)
 
         def choiceselected():
             print("button pressed")
@@ -17,25 +28,20 @@ class App(ctk.CTk):
         button = ctk.CTkButton(master=self, text="", command=choiceselected)
         button.pack(padx=20, pady=10)
 
-        number = ctk.CTkLabel(self,text="Current grade value: 50%")
+        number = ctk.CTkLabel(self, text="Current grade value: 50%")
 
         def slider_event(value):
-            value = round(value)
-            percent = "Current grade value: " + str(value) + "%"
+            percent = f"Current grade value: {value:.0f}%"
             number.configure(text=percent)
-            
 
         number.pack()
-        slider = ctk.CTkSlider(master=self, from_=0, to=100, command=slider_event)
-        slider.place(relx=0.5, rely=0.2, anchor=ctk.N)
-        
-        
-
-
-
-
-
-        
+        # slider = ctk.CTkSlider(
+        #     self,
+        #     from_=0,
+        #     to=100,
+        #     command=slider_event
+        # )
+        # slider.place(relx=0.5, rely=0.2, anchor=ctk.N)
 
 
 def main():
--- ui.py
+++ ui.py
@@ -1,32 +1,17 @@
 import customtkinter as ctk
 
 
-class App(ctk.CTk):
-    def __init__(self) -> None:
-        super().__init__()
+def auth(root: ctk.CTk):
+    usernamehead = ctk.CTkLabel(root, text="Enter username here:")
+    username = ctk.CTkEntry(root, width=300)
+    usernamehead.pack()
+    username.pack()
 
-        usernamehead = ctk.CTkLabel(self,text="Enter username here:")
-        username = ctk.CTkEntry(self, width=300)
-        usernamehead.pack()
-        username.pack()
+    passwordhead = ctk.CTkLabel(root, text="Enter password here:")
+    password = ctk.CTkEntry(root, width=300, show="*")
+    passwordhead.pack()
+    password.pack()
 
-        passwordhead = ctk.CTkLabel(self,text="Enter password here:")
-        password = ctk.CTkEntry(self, width=300, show="*")
-        passwordhead.pack()
-        password.pack()
-        
-        authenticate_button = ctk.CTkButton(self, text="Authenticate")
-        authenticate_button.pack()
-
-        
-        self.destroy()
-
-
-def main():
-    ctk.set_appearance_mode("dark")
-    App().mainloop()
-
-
-if __name__ == "__main__":
-    main()
+    authenticate_button = ctk.CTkButton(root, text="Authenticate")
+    authenticate_button.pack()
 
