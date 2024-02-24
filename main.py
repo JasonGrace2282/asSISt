c5b159f0e3590ca85147f9c3df6bd997626d084a
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-24 11:41:06 -0500

    Basics python project

---
+++ main.py
@@ -0,0 +1,15 @@
+import customtkinter as ctk
+
+
+class App(ctk.CTk()):
+    def __init__(self) -> None:
+        super().__init__()
+        # code goes here
+
+
+def main():
+    App().mainloop()
+
+
+if __name__ == "__main__":
+    main()
