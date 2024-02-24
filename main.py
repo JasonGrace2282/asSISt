60ca38a10777f7a56777561417acb4d2f08f9836
justiny1137 <justiny1137@gmail.com>
2024-02-24 13:49:44 -0500

    set up slider and current value function

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import customtkinter as ctk
+ 
 
 
 class App(ctk.CTk):
     def __init__(self) -> None:
         super().__init__()
+        self.title("Hello World")
+        self.geometry("500x500")
+
+        
 
 
-        usernamehead = ctk.CTkLabel(self,Text="Enter User name here")
-        username = ctk.CTkEntry(self, width=300)
-        usernamehead.pack()
-        username.pack()
+        number = ctk.CTkLabel(self,text="100")
+
+        def slider_event(value):
+            value = round(value)
+            percent = "Current grade value: " + str(value) + "%"
+            number.configure(text=percent)
+            
 
 
+
+        number.pack()
+        slider = ctk.CTkSlider(master=self, from_=0, to=100, command=slider_event)
+        slider.place(relx=0.5, rely=0.5, anchor=ctk.N)
+        
+        
 
 
 
