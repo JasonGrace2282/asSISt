4307c63ce5e934786d68f3711fc888efe1f2f76e
arjung05 <arjungarghd@gmail.com>
2024-02-24 12:42:29 -0500

    username/password

---
+++ ui.py
@@ -0,0 +1,23 @@
+import customtkinter as ctk
+
+class App(ctk.CTk):
+    def __init__(self) -> None:
+        super().__init__()
+
+        usernamehead = ctk.CTkLabel(self,text="Enter username here:")
+        username = ctk.CTkEntry(self, width=300)
+        usernamehead.pack()
+        username.pack()
+
+        passwordhead = ctk.CTkLabel(self,text="Enter password here:")
+        password = ctk.CTkEntry(self, width=300, show="*")
+        passwordhead.pack()
+        password.pack()
+
+def main():
+    ctk.set_appearance_mode("dark")
+    App().mainloop()
+
+if __name__ == "__main__":
+    main()
+
