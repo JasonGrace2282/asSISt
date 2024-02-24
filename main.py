6c609a0fc44d384dcd037ef1a80996cf55a96eae
justiny1137 <justiny1137@gmail.com>
2024-02-24 14:10:20 -0500

    change rely

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ class App(ctk.CTk):
         self.title("Hello World")
         self.geometry("500x500")
 
-        
+        title1 = ctk.CTkLabel(self,text="Calculate the impact of an new individual assginment on your grade based on its weight and score")
+        title2 = ctk.CTkLabel(self,text="Instructions: Pick whether to enter your grade as number of right points or as a percentage.")
 
+        def choiceselected():
+            print("button pressed")
 
-        number = ctk.CTkLabel(self,text="100")
+        button = ctk.CTkButton(master=self, text="", command=choiceselected)
+        button.pack(padx=20, pady=10)
+
+        number = ctk.CTkLabel(self,text="Current grade value: 50%")
 
         def slider_event(value):
             value = round(value)
@@ -23,7 +29,7 @@ class App(ctk.CTk):
 
         number.pack()
         slider = ctk.CTkSlider(master=self, from_=0, to=100, command=slider_event)
-        slider.place(relx=0.5, rely=0.5, anchor=ctk.N)
+        slider.place(relx=0.5, rely=0.2, anchor=ctk.N)
         
         
 
