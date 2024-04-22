74ac90b4b7a57c04c27b8fd7147924544448c302
Ansh Agrawal <hsna.agrawal@gmail.com>
2024-04-22 17:32:12 -0400

    Ported login page to bootstrap

--- login/forms.py
+++ login/forms.py
@@ -2,13 +2,13 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=20)
+    username = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': "form-control"}))
     password = forms.CharField(
-        widget=forms.PasswordInput,
+        widget=forms.PasswordInput(attrs={'class': "form-control"}),
         max_length=100
     )
     domain = forms.CharField(
         max_length=200,
         required=False,
-        widget=forms.TextInput(attrs={"placeholder": "sisstudent.fcps.edu/SVUE"})
+        widget=forms.TextInput(attrs={"placeholder": "sisstudent.fcps.edu/SVUE", "class": "form-control mb-3"})
     )
